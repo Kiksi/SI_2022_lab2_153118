5a072f725e39622a9ab2b92b9193b4c3985f1732
Kiksi <karsov7@gmail.com>
2022-05-21 15:16:39 +0200

    Initial commit

---
+++ SI_lab2_153118/src/test/java/SILab2Test.java
@@ -0,0 +1,26 @@
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class SILab2Test {
+    @Test
+    public void test1() {
+        assertEquals(SILab2.function(Arrays.asList("0", "#", "#", "#", "0", "#", "0", "0","#")), Arrays.asList("2", "#", "#", "#", "3", "#", "1", "1", "#"));  
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test2() {
+        assertEquals(SILab2.function(new ArrayList<String>()), new IllegalArgumentException("List length should be greater than 0"));
+         
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void test3() {
+        assertEquals(SILab2.function(Arrays.asList("0", "#", "#", "#", "0", "#", "0", "0")), new IllegalArgumentException("List length should be a perfect square"));
+         
+    }
+}
